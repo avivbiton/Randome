4af186a751c74113651933b172f65c1758e4d5c2
Aviv Biton <avivbiton01@gmail.com>
2019-08-05 10:38:11 +0300

    Browse search state is now based on search query

--- client/src/Components/Pages/browseRandomizerPage/Browse.js
+++ client/src/Components/Pages/browseRandomizerPage/Browse.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import Display from "./Display";
 import API from "../../../API/api";
+import queryString from "query-string";
 
 const SORT_TYPES = {
     LATEST: "createdAt",
@@ -9,28 +11,48 @@ const SORT_TYPES = {
     RECENTLY_UPDATED: "updatedAt"
 };
 
-export default function Browse() {
+function Browse({ location, history }) {
 
     const [items, setItems] = useState(null);
-    const [sortType, setSort] = useState(SORT_TYPES.LATEST);
     const [filter, setFilter] = useState("");
     const [error, setError] = useState(null);
 
+    useEffect(function setDefaultQuery() {
+        const currentQuery = queryString.parse(location.search);
+        if (currentQuery.page && currentQuery.sort) return;
+        history.push({
+            search: queryString.stringify({
+                page: currentQuery.page || 0,
+                sort: currentQuery.sort || SORT_TYPES.LATEST
+            })
+        });
+    }, []);
+
     useEffect(() => {
+        const query = queryString.parse(location.search);
+        if (!query.page || !query.sort) return;
         setItems(null);
         setError(null);
-        fetchItems();
-    }, [sortType]);
+        fetchItems(query.page, query.sort);
+    }, [location.search]);
 
-    async function fetchItems() {
-        const itemsData = await API.randomizers.fetch(0, sortType);
-        console.log(itemsData);
+
+    async function fetchItems(page, sort) {
+        const itemsData = await API.randomizers.fetch(page, sort);
         if (itemsData) {
             setItems(itemsData);
         } else {
             setError("Could not retrive items, Please try again later.");
         }
     }
+
+
+    function setSort(sortType) {
+        const currentQuery = queryString.parse(location.search);
+        currentQuery.sort = sortType;
+        history.push({ search: queryString.stringify(currentQuery) });
+    }
+
     return (
         <div className="container-fluid">
             <div className="row mt-4">
@@ -66,3 +88,4 @@ export default function Browse() {
 }
 
 
+export default withRouter(Browse);
