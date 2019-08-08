6ddbe2a2cb100e79b9482c9739773353bc328475
Aviv Biton <avivbiton01@gmail.com>
2019-08-08 19:27:23 +0300

    Added get account info route

---
+++ src/routes/account/getAccount.js
@@ -0,0 +1,15 @@
+const authenticateUser = require("../../middleware/authenticateUser");
+
+const getAccount = [
+    authenticateUser,
+    (req, res, next) => {
+        return res.json({
+            adminLevel: req.account.adminLevel,
+            likes: req.account.meta.likes,
+            favorites: req.account.meta.favorites,
+            randomizers: req.account.randomizers
+        });
+    }
+];
+
+module.exports = getAccount;
