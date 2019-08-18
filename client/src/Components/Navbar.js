7f3187601990d02fe9fc6caf0800977ad1c71fac
Aviv Biton <avivbiton01@gmail.com>
2019-08-19 00:49:00 +0300

    Set default image if photoURL is invalid

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -5,7 +5,7 @@ import { logOutUser } from "../Authentication/auth";
 
 function Navbar() {
     const user = useSelector(state => {
-        if(state.auth.user == null) return null;
+        if (state.auth.user == null) return null;
         return {
             displayName: state.auth.user.displayName,
             photoURL: state.auth.user.photoURL
@@ -50,7 +50,8 @@ function Navbar() {
 function UserDropdown({ user }) {
     return (
         <div className="dropdown mx-2">
-            <img className="nav-item dropdown-toggle rounded-circle navbar-brand" style={{ width: "3rem", maxHeight: "3.5rem" }} src={user.photoURL} id="dropdownMenuButton" alt="user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+            <img className="nav-item dropdown-toggle rounded-circle navbar-brand" style={{ width: "3rem", maxHeight: "3.5rem" }} src={user.photoURL} id="dropdownMenuButton" alt="user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                onError={e => e.target.src = "/favicon.ico"}/>
             <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
                 <h5 className="border-bottom"><Link to="/profile" className="dropdown-item">{user.displayName}</Link></h5>
                 <Link className="dropdown-item" to="/profile">Profile</Link>
