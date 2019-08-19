d678f996dbe31f051d69a0e2b11bb5b32483e6a8
Aviv Biton <avivbiton01@gmail.com>
2019-08-19 11:37:10 +0300

    Drag scroll added

---
+++ client/src/Components/DragScroll.js
@@ -0,0 +1,52 @@
+import React, { useState, useCallback, useRef, useMemo } from "react";
+
+export default function DragScroll({ className, children }) {
+
+    const [dragging, setDragging] = useState(false);
+    const [lastPosition, setPosition] = useState({ x: 0, y: 0 });
+    const container = useRef(null);
+
+    const isMobile = useMemo(() => {
+        return isDeviceMobile();
+    }, []);
+
+    const mouseUp = useCallback(() => {
+        if (dragging) {
+            setDragging(false);
+        }
+    }, [dragging]);
+
+    const mouseDown = useCallback(e => {
+        if (dragging === false) {
+            setDragging(true);
+            setPosition({ x: e.clientX, y: e.clientY });
+        }
+    }, [dragging]);
+    const mouseMove = useCallback(e => {
+        if (dragging) {
+            container.current.scrollLeft -= (-lastPosition.x + e.clientX);
+            container.current.scrollTop -= (-lastPosition.y + e.clientY);
+            setPosition({ x: e.clientX, y: e.clientY });
+        }
+    }, [container, dragging, lastPosition]);
+
+    if(isMobile){
+        return <div className={className}>{children}</div>
+    }
+    
+    return (
+        <div className={className}
+            onMouseUp={mouseUp}
+            onMouseDown={mouseDown}
+            onMouseMove={mouseMove}
+            ref={container}>
+            {children}
+        </div>
+    );
+
+}
+
+
+function isDeviceMobile() {
+    return window.matchMedia("(max-width: 992px)").matches;
+}
--- client/src/Components/FeaturedItems.js
+++ client/src/Components/FeaturedItems.js
@@ -4,6 +4,7 @@ import LoadingSpinner from "./LoadingSpinner";
 import API from "../API/api";
 import toastr from "toastr";
 import { toastrDefault } from "../config";
+import DragScroll from "./DragScroll";
 
 import { SORT_TYPES } from "./Pages/browseRandomizerPage/Browse";
 
@@ -27,11 +28,11 @@ export default function FeaturedItems() {
     if (featured === null) return <div className="d-flex justify-content-center"><LoadingSpinner size="lg" /></div>
 
     return (
-        <section className="container-fluid mt-4 mt-lg-0">
+        <section className="container-fluid mt-4 mt-lg-0 dragscroll">
             <div className="text-center">
                 <h1 className="border border-primary rounded py-4">Featured</h1>
                 <hr />
-                <div className="d-flex flex-row overflow-auto scrollbar">
+                <DragScroll className="d-flex flex-row overflow-auto scrollbar dragscroll">
                     {featured.map(i =>
                         <div key={i._id} className="m-1">
                             <ItemDisplay
@@ -45,7 +46,7 @@ export default function FeaturedItems() {
                         </div>
                     )}
 
-                </div>
+                </DragScroll>
             </div>
         </section>
     );
