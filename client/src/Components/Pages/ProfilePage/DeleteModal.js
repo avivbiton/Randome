92650ea6386dd3af49cc6be14f45f9ae430ef005
Aviv Biton <avivbiton01@gmail.com>
2019-08-21 10:43:33 +0300

    Deleted unused imports

--- client/src/Components/Pages/ProfilePage/DeleteModal.js
+++ client/src/Components/Pages/ProfilePage/DeleteModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useCallback } from "react";
 import { Modal } from "react-bootstrap";
 import api from "../../../API/api";
 import toastr from "toastr";
