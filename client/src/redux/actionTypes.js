ddd544ea1cd26c397045671cf1429ae1f7cb3c87
Aviv Biton <avivbiton01@gmail.com>
2019-09-04 12:50:27 +0300

    Remove unused error reducer and types

--- client/src/redux/actionTypes.js
+++ client/src/redux/actionTypes.js
@@ -1,9 +1,3 @@
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const SET_ACCOUNT = "SET_ACCOUNT";
 export const LOGOUT = "LOGOUT";
-
-
-export const PUSH_ERROR = "PUSH_ERROR";
-export const PUSH_ERROR_ARRAY = "PUSH_ERROR_ARRAY";
-export const CLEAR_ERRORS = "CLEAR_ERRORS";
-export const REMOVE_ERROR = "REMOVE_ERROR";
