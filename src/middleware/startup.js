f9b1c47e8cddeeb107b6ec82886b709c30bd0d6c
Aviv Biton <avivbiton01@gmail.com>
2019-09-04 11:58:41 +0300

    Add helmet middleware for increased security

--- src/middleware/startup.js
+++ src/middleware/startup.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
-
+const helmet = require("helmet");
 const useMiddleware = (app) => {
+    app.use(helmet());
     app.use(logger("dev"));
     app.use(express.json({ limit: "500kb" }));
     app.use(express.urlencoded({ extended: false, limit: "500kb" }));
