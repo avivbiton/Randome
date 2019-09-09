e4b765d96692d8fbda99c3b0c1e63573807041ba
Aviv Biton <avivbiton01@gmail.com>
2019-09-09 11:52:04 +0300

    Update env variables for deployment

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,27 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-admin.json");
 
 const initialize = () => {
+
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert(getServiceAccount()),
         databaseURL: process.env.FIREBASE_DB
     });
+};
 
+
+const getServiceAccount = () => {
+    return {
+        type: process.env.FIREBASE_ADMIN_TYPE,
+        project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_ADMIN_KEY_ID,
+        private_key: process.env.FIREBASE_ADMIN_SECRET_KEY,
+        client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+        token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER,
+        client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_CERT_URL
+    };
 };
 
 module.exports = { initialize };
