3320f14505ca3d6e81930cacafdbf4983f8e9fb3
Aviv Biton <avivbiton01@gmail.com>
2019-09-09 13:07:05 +0300

    Update  proxy to allow heroku to send requets correctly

--- src/production.js
+++ src/production.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const initializeProductionBuild = (app) => {
     const combinedPath = path.join(__dirname + "/..", "client", "build");
-    console.log(combinedPath);
     app.use(express.static(combinedPath));
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname + "/..", "client", "build", "index.html"));
