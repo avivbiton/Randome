9530d2a240c4fe1b3494657e0ed5df11248708cf
Aviv Biton <avivbiton01@gmail.com>
2019-09-09 14:18:00 +0300

    Update debugging

--- src/middleware/authenticateUser.js
+++ src/middleware/authenticateUser.js
@@ -3,28 +3,38 @@ const AuthenticationError = require("../Errors/AuthenticationError");
 const Account = require("../Models/Account");
 
 module.exports = (required = true) => async (req, res, next) => {
-    const authToken = req.get("Authorization");
     try {
-        const decoded = await admin.auth().verifyIdToken(authToken);
-        req.user = decoded;
-        console.log(req.user);
+        console.log("require auth");
+        const authToken = req.get("Authorization");
+        console.log("auth token " + authToken);
         try {
-            console.log("finding account");
-            const data = await Account.find({ userId: decoded.uid }).lean().exec();
-            console.log("found " + data);
-            req.account = data[0];
-        } catch (error) {
-            console.log(error);
-            req.account = null;
-        }
-        next();
-    } catch (error) {
-        if (required)
-            next(new AuthenticationError());
-        else {
-            req.account = null;
-            req.user = null;
+            const decoded = await admin.auth().verifyIdToken(authToken);
+            req.user = decoded;
+            console.log(req.user);
+            try {
+                console.log("finding account");
+                const data = await Account.find({ userId: decoded.uid }).lean().exec();
+                console.log("found " + data);
+                req.account = data[0];
+            } catch (error) {
+                console.log(error);
+                req.account = null;
+            }
             next();
+        } catch (error) {
+            if (required)
+                next(new AuthenticationError());
+            else {
+                req.account = null;
+                req.user = null;
+                next();
+            }
         }
     }
+    catch (error) {
+        console.log("failed to get auth header");
+        console.error(error);
+        next(new AuthenticationError());
+    }
+
 };
