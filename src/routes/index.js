87a4aae3568f9f2c53b78f31dac2f0a1bee00b38
Aviv Biton <avivbiton01@gmail.com>
2019-09-13 10:31:07 +0300

    Change backend API routes to include /api/ prefix

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const accountIndex = require("./account/accountIndex");
 const errorHandler = require("./errorHandler");
 
 function LoadRoutes(app) {
-    app.use("/randomizer", randomizerIndex);
-    app.use("/account", accountIndex);
+    app.use("/api/randomizer", randomizerIndex);
+    app.use("/api/account", accountIndex);
 
     app.use(errorHandler);
 }
