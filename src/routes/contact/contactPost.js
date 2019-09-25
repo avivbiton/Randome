ede83b2ecf4ed54f1e40c3f5c1988d30113e5757
Aviv Biton <avivbiton01@gmail.com>
2019-09-25 11:08:31 +0300

    Add rate limiting to contact requests

--- src/routes/contact/contactPost.js
+++ src/routes/contact/contactPost.js
@@ -2,6 +2,7 @@ const requireBody = require("../../middleware/requireBody");
 const validateBody = require("../../middleware/validateBodyMatchSchema");
 const authenticateUser = require("../../middleware/authenticateUser");
 const ContactRequest = require("../../Models/ContactRequest");
+const contactLimiter = require("../../rateLimiters/contactLimiter");
 
 const validationSchema = require("../../Validation/contact");
 
@@ -9,6 +10,7 @@ module.exports = [
     requireBody(["title", "email", "message"]),
     validateBody(validationSchema),
     authenticateUser(false),
+    contactLimiter,
     async (req, res, next) => {
         try {
             const { title, email, message } = req.body;
@@ -18,10 +20,10 @@ module.exports = [
                 message,
                 user: req.user ? req.user.displayName : null
             });
-
+            
             return res.status(203).json({ success: "Request sent." });
         } catch (error) {
             next(error);
         }
     }
-]
+];
