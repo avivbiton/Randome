8e494112052f71e1d630c63504d626fd46dbbb4e
Aviv Biton <avivbiton01@gmail.com>
2019-10-02 13:44:06 +0300

    Update to use getName() instead of using the constructor name

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,15 +1,5 @@
 import { ContentGenerator } from "randomcontentgenerator";
-
 export function convertTypeToName(field) {
-    
-    const name = ContentGenerator.findParser(field).constructor.name;
-    switch (name) {
-        case "BasicParser":
-            return "Basic Picker";
-        case "MinMaxParser":
-            return "Min-Max Picker";
-        case "MultiPickerParser":
-            return "Multi Picker";
-        default: throw new Error("Invalid parser");
-    }
+
+    return ContentGenerator.findParser(field).getName();
 }
